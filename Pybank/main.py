b74e0319ecbab15f4bf2998a0ba746578822b1ab
Daniel <danielanguiano2017@gmail.com>
2022-11-08 14:32:14 -0600

    final edit

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,4 +1,4 @@
-
+#Import all programs needed to run this code ie : os and csv
 import os
 import csv
 
@@ -7,29 +7,37 @@ import csv
 print("Financial Analysis")
 print(" ")
 print("---------------------------------")
-
+#Import csv using os.path
 csvpath = os.path.join("Resources", "budget_data.csv")
-
+#create lists for total votes, and total votes for each candidate
+# create varibales to track total proft, average change month to month, greatest increase
+# and greatest decrease
 Months=[]
 Total = 0
 Change = 0
 Avgchge = 0
 increase = 0
 decrease = 0
+#open csv as a csv file and make sure header matches data style
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-
+#append vote count to count list
+# track total net for the year 
     for row in csvreader:
         month = row[0]
         Months.append(month)
         profit = int(row[1])
         Total = Total + profit
+        # create a code that does not account for the header to track change month 
+        # to month so we can then find average change
         if row[1] != 0:
 
             Change = Change + int(row[1])
+            # write if statement to track greatest increase as well as greatest decrease
             if int(Change) > increase:
                 increase = int(Change)
                 day = str(row [0])
+            
             if int(Change) < decrease:
                 decrease = int(Change)
                 days = str(row[0])
@@ -37,9 +45,10 @@ with open(csvpath) as csvfile:
             Avgchge = Avgchge + Change
             Change = -int(row[1])
         
-            
+    #write code that counts total month using len function     
     print("Total Months: " + str(int(len(Months) -1)))
     print(" ")
+    # write print statements that displays all results in Anlaysis Results
     print("Total: $" + str(int(Total)) )
     print(" ")
     print("Average Change: $" + str(format(float((Avgchge - 1088983)/(int(len(Months))-2)), '.2f')))
@@ -49,7 +58,7 @@ with open(csvpath) as csvfile:
     print("Greatest Decrease in Profits: " + str(days) + " ($" + str(decrease) +")")
     
 
-   
+   #declare variables to include as outputs for file export
     aa = " Financial Analysis"
     bb = "\n "
     ab = "\n "
@@ -68,8 +77,11 @@ with open(csvpath) as csvfile:
     h = " \n"
     hg = "\n "
     i = "Greatest Decrease in Profits: " + str(days) + " ($" + str(decrease) +")"
-    txt = os.path.join("Analysis")
+    
+    # write a with statement to add file to Analysis folder named: "PYTHON_ANALYSIS"
     with open( "Analysis/PYTHON_ANALYSIS", "w") as file:
+        # Now that we have variables, assign them to a list and 
+        # then use 'writelines' to write these results
         lines = ([aa,bb,ab,cc,bc,a,b,ba,c,d,dc,e,f,fe,g,h,hg,i])
         file.writelines(lines)
 
--- Pypoll/main_poll.py
+++ Pypoll/main_poll.py
@@ -1,77 +1,67 @@
-import os
 import csv
+import os
 
 #Print "Election Results"
 print(" Election Results", "\n","\n","------------------------","\n")
 
 
-#Print "-----------------------"
+#Wright "-----------------------"
 
-#Write code that coutns all votes, might need to use the length of total counts
+#Import csv using os.path
 
 csvpath = os.path.join("Resources", "election_data.csv")
-
+#create lists for total votes, and total votes for each candidate
 Count = []
 Charles = []
 Diana = []
 Raymon = []
-
-
-
-
-
+#open csv as a csv file and make sure header matches data style
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-
+#append vote count to count list
     for row in csvreader:
         counts = row[0]
         Count.append(counts)
+        # write if statement to append vote count to individual candidate lists
         if row[2] == "Charles Casper Stockham":
             Charles_count = row[2]
             Charles.append(Charles_count)
+        
         elif row[2] == "Diana DeGette":
             Diana_count = row[2]
             Diana.append(Diana_count)
+        
         elif row[2] == "Raymon Anthony Doane":
             Raymon_count = row[2]
             Raymon.append(Raymon_count)
+    # write if statement to declare the winner of the election
     if len(Raymon) > (len(Charles) and len(Diana)):
-        winner = str("Raymon Anthony Doane")
+        dinners = str("Raymon Anthony Doane")
+    
     if len(Charles) > (len(Raymon) and len(Diana)):
-        winner = str("Charles Casper Stockham")
+        dinners = str("Charles Casper Stockham")
+    
     if len(Diana) > (len(Charles) and len(Raymon)):
-        winner = str("Diana DeGette")
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
+        dinners = str("Diana DeGette")
+#Write code that coutns all votes, might need to use the length of total counts
 print(" Total Votes: " + str(int(len(Count) -1)) + "\n" + "\n" 
 + " ----------------------" + "\n" + "\n")
-
-
 print(" Charles Casper Stockham: " + str(format((int(len(Charles))/int(len(Count) -1))*100,'.3f') + "% ") + "(" + str(int(len(Charles))) + ")" +"\n")
 print(" Diana DeGette: " + str(format((int(len(Diana))/int(len(Count) -1))*100,'.3f') + "% ") + "(" + str(int(len(Diana))) + ")" +"\n" )
 print(" Raymon Anthony Doane: " + str(format((int(len(Raymon))/int(len(Count) -1))*100,'.3f') + "% ") + "(" + str(int(len(Raymon))) + ")" + "\n")
-print(" --------------------" + "\n", "\n" + " Winner: " + str(winner) + "\n", "\n" + " ------------------------" + "\n")
-
+print(" --------------------" + "\n", "\n" + " Winner: " + str(dinners) + "\n", "\n" + " ------------------------" + "\n")
+#declare variables to include as outputs for file export
 a = " Total Votes: " + str(int(len(Count) -1)) + "\n" + "\n" + " ----------------------" + "\n" + "\n"
 b = " Charles Casper Stockham: " + str(format((int(len(Charles))/int(len(Count) -1))*100,'.3f') + "% ") + "(" + str(int(len(Charles))) + ")" +"\n" + "\n"
 c = " Diana DeGette: " + str(format((int(len(Diana))/int(len(Count) -1))*100,'.3f') + "% ") + "(" + str(int(len(Diana))) + ")" +"\n" +"\n" 
 d = " Raymon Anthony Doane: " + str(format((int(len(Raymon))/int(len(Count) -1))*100,'.3f') + "% ") + "(" + str(int(len(Raymon))) + ")" + "\n" +"\n"
-e = " --------------------" + "\n" + "\n" + " Winner: " + str(winner) + "\n"+ "\n" + " ------------------------" + "\n"
+e = " --------------------" + "\n" + "\n" + " Winner: " + str(dinners) + "\n"+ "\n" + " ------------------------" + "\n"
 f =" Election Results"+ "\n"+"\n"+"------------------------"+"\n"+"\n"
+# write a with statement to add file to Analysis folder named: "PYTHON_ANALYSIS-PYPOLL"
 txt = os.path.join("Analysis")
 with open("Analysis/PYTHON_ANALYSIS-PYPOLL", "w") as file:
+    # Now that we have variables, assign them to a list and cd ..
+    # then use 'writelines' to write these results
     lines = ([f,a,b,c,d,e])
     file.writelines(lines)
 
