d12d75d478263d171143ebb12aa95011ee66d6d4
Daniel <danielanguiano2017@gmail.com>
2022-11-07 22:38:44 -0600

    Commiting new changes

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,6 +1,7 @@
 
 import os
 import csv
+
 # print the analysis name and seperating border for the analysis
 
 print("Financial Analysis")
@@ -56,6 +57,50 @@ with open(csvpath) as csvfile:
     print(" ")
     print("Greatest Decrease in Profits: " + str(days) + " ($" + str(decrease) +")")
     
+
+   
+    aa = " Financial Analysis"
+    bb = "\n "
+    ab = "\n "
+    cc = "---------------------------------"
+    bc = "\n "
+    a = "Total Months: " + str(int(len(Months) -1))
+    b = "\n "
+    ba= "\n "
+    c = "Total: $" + str(int(Total))
+    d = "\n"
+    dc= "\n "
+    e = "Average Change: $" + str(format(float((Avgchge - 1088983)/(int(len(Months))-2)), '.2f'))
+    f = "\n"
+    fe ="\n "
+    g = "Greatest Increase in Profits: " + str(day) + " ($" + str(increase) +")"
+    h = " \n"
+    hg = "\n "
+    i = "Greatest Decrease in Profits: " + str(days) + " ($" + str(decrease) +")"
+    txt = os.path.join("Analysis", "y")
+    with open( txt, "w") as file:
+        lines = ([aa,bb,ab,cc,bc,a,b,ba,c,d,dc,e,f,fe,g,h,hg,i])
+        file.writelines(lines)
+
+
+        
+
+
+        
+
+        
+
+
+
+
+
+
+     
+
+
+        
+
+
 
 
         
